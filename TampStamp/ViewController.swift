bec51d3e7021cd74d180f8e81fa3267af5b7b00a
mahirooo <mahiro.nagasaki@gmail.com>
2022-09-16 17:57:25 +0900

    TabBar中央ボタン見た目完成

--- TampStamp/ViewController.swift
+++ TampStamp/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UITabBar.appearance().tintColor = UIColor(red: 0.945, green: 0.356, blue: 0.411, alpha: 1.0)
     }
+    
 
 
 }
+
+
 
