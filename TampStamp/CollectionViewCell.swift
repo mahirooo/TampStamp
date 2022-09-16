7b4c7eff7a216f7b471108374bd1974311035c24
mahirooo <mahiro.nagasaki@gmail.com>
2022-09-16 18:41:58 +0900

    collectionviewのおおもとは実装DONE

---
+++ TampStamp/CollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewCell.swift
+//  TampStamp
+//
+//  Created by 長崎茉優 on 2022/09/16.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var todoLabel: UILabel!
+    @IBOutlet var rewardLabel: UILabel!
+    @IBOutlet var backgroundImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundImageView.layer.cornerRadius = 12   
+        // Initialization code
+    }
+
+}
