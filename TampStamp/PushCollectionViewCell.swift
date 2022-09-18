9b9e6c9e99523ed4c64eeb614f4b6806e80c311a
mahirooo <mahiro.nagasaki@gmail.com>
2022-09-18 23:47:52 +0900

    スタンプが表示されない

---
+++ TampStamp/PushCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PushCollectionViewCell.swift
+//  TampStamp
+//
+//  Created by 長崎茉優 on 2022/09/18.
+//
+
+import UIKit
+
+class PushCollectionViewCell: UICollectionViewCell {
+    
+}
--- TampStamp/ViewController.swift
+++ TampStamp/ViewController.swift
@@ -9,13 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var horizontalCollectionView: UICollectionView!
+
+    var viewWidth: CGFloat!
+    var viewHeight: CGFloat!
+    var cellWidth: CGFloat!
+    var cellHeight: CGFloat!
+    var cellOffset: CGFloat!
+    var navHeight: CGFloat!
+
+    var photoArray = ["cardDesignBlue","cardDesignPink1","cardDesignPink2"]
+    var rewardArray = ["フラペチーノ","スプラ","焼肉"]
+    var todoArray = ["腹筋100回","毎日自炊","お風呂掃除"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        viewWidth = view.frame.width
+        viewHeight = view.frame.height
+        navHeight = self.navigationController?.navigationBar.frame.size.height
+
+        horizontalCollectionView.delegate = self
+        horizontalCollectionView.dataSource = self
         UITabBar.appearance().tintColor = UIColor(red: 0.945, green: 0.356, blue: 0.411, alpha: 1.0)
+        let nib = UINib(nibName: "CollectionViewCell", bundle: .main)
+        horizontalCollectionView.register(nib, forCellWithReuseIdentifier: "cell")
+
+    }
+
+}
+
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoArray.count
+//        cellの数を決める
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.backgroundColor = UIColor.white
+        cell.layer.cornerRadius = 30
+        cell.layer.masksToBounds = false
+        cell.rewardLabel.text = "ご褒美：\(rewardArray[indexPath.row])"
+        cell.todoLabel.text = "目標：\(todoArray[indexPath.row])"
+        cell.backgroundImageView.image = UIImage(named: photoArray[indexPath.row])
+        return cell
+
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+     
+            // セルの選択を解除
+        collectionView.cellForItem(at: indexPath)
+     
+            // 別の画面に遷移
+            performSegue(withIdentifier: "toNextViewController", sender: nil)
+        }
+    
+//    func collectionView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: false)
+//
+//        //　タップされたセルの取得
+//        let cell = self.tableView.cellForItem(at:indexPath)
+//
+//        // セルの背景色を変更する
+//
+//      }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 50
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        cellWidth = viewWidth/0.8
+//        cellHeight = viewHeight-690
+//        return CGSize(width: cellWidth, height: cellHeight)
+//    }
 
 }
 
